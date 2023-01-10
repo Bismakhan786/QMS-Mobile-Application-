0f3d849d181ef4f4e4259e75a64264511a731408
Bisma Khan <73794243+Bismakhan786@users.noreply.github.com>
2023-01-10 17:50:05 +0500

    removed extra code

--- backend.js
+++ backend.js
@@ -1,5 +1,3 @@
-// var arrivalArr = [0, 2, 4, 6, 6, 7, 8, 10, 14, 14];
-// var serviceTimeArr = [1, 3, 4, 2, 7, 5, 1, 3, 4, 2];
 
 //InterArrival Time Calculation Function
 function interArrivalCalculation(arrivalArr) {
@@ -15,54 +13,11 @@ function interArrivalCalculation(arrivalArr) {
 function timeCalculation(arr1, arr2) {
   let resultantTimeArr = [];
   for (let i = 0; i < arr1.length; i++) {
-
-  // let val = arr1[i] - arr2[i]
-  // let add_val =0 
-  // let len = 0
-  // for (let i=0;i<arr.length;i++){
-  //   if((arr1[i] - arr2[i])>mins){
-  //     break
-  //   }
-  //   else{
-  //     val += arr[i]
-  //     len = i
-  //   }
-  // }
-  // return add_val/len 
-
     resultantTimeArr.push(Math.abs(arr1[i] - arr2[i]));
   }
   return resultantTimeArr;
 }
 
-// //TurnAround Time Calculation Function
-// function turnAroundTimeCalculation(endingTimeArr,arrivalArr){
-//     let turnAroundTimeArr = []
-//     for(let i = 0; i<arrivalArr.length; i++){
-//         turnAroundTimeArr.push(endingTimeArr[i]-arrivalArr[i])
-//     }
-//     return turnAroundTimeArr
-// }
-
-// //Waiting Time Calculation Function
-// function waitTimeCalculation(turnAroundTimeArr,serviceTimeArr){
-//     let waitTimeArr = []
-//     for(let i = 0; i<turnAroundTimeArr.length; i++){
-//         waitTimeArr.push(turnAroundTimeArr[i]-serviceTimeArr[i])
-//     }
-//     return waitTimeArr
-// }
-
-// //Response Time Calculation Function
-// function responseTimeCalculation(startTimeArr,arrivalArr){
-//     let responseTimeArr = []
-//     for(let i = 0; i<arrivalArr.length; i++){
-//         responseTimeArr.push(startTimeArr[i]-arrivalArr[i])
-//     }
-//     return responseTimeArr
-// }
-
-//Start And End Time Calculation Function
 
 function startEndArrCalculation(arrivalArr, serviceTimeArr) {
   let startTime = [];
@@ -95,40 +50,9 @@ function startEndArrCalculation(arrivalArr, serviceTimeArr) {
 
 //Avg InterArrival,Waiting,Service,TurnAround Time Calculation
 function avgTime(arr) {
-  // let add_val = 0 
-  // let len = 0
-  // for (let i=0;i<arr.length;i++){
-  //   if(arr[i]>mins){
-  //     break
-  //   }
-  //   else{
-  //     add_val += arr[i]
-  //     len = i
-  //   }
-  // }
-  // return add_val/len
   return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 
-// //Avg InterArrival Time Calculation
-// function avgInterArrivalTime(interArrivalArr){
-//     return interArrivalArr.reduce((a, b) => a + b, 0) / interArrivalArr.length
-// }
-
-// //Avg Waiting Time Calculation
-// function avgWaitingTime(waitTimeArr){
-//     return waitTimeArr.reduce((a, b) => a + b, 0) / waitTimeArr.length
-// }
-
-// //Avg Service Time Calculation
-// function avgServiceTime(serviceTimeArr){
-//     return serviceTimeArr.reduce((a, b) => a + b, 0) / serviceTimeArr.length
-// }
-
-// //Avg TurnAround Time Calculation
-// function avgTurnAroundTime(turnAroundTimeArr){
-//     return turnAroundTimeArr.reduce((a, b) => a + b, 0) / turnAroundTimeArr.length
-// }
 
 //Average Waiting time for those who wait
 
@@ -148,50 +72,6 @@ function avgUtilizationRate(startTimeIdle, endTimeIdle, endTimeLastVal) {
   return Math.round((1 - idle / endTimeLastVal) * 100);
 }
 
-function main() {
-  var interArrivalArr = interArrivalCalculation(arrivalArr);
-  var startEnd = startEndArrCalculation(arrivalArr, serviceTimeArr);
-  var turnAroundTimeArr = timeCalculation(startEnd[1], arrivalArr);
-  var waitTimeArr = timeCalculation(turnAroundTimeArr, serviceTimeArr);
-  var responseTimeArr = timeCalculation(startEnd[0], arrivalArr);
-
-  var startTime = startEnd[0];
-  var endTime = startEnd[1];
-
-  var startTimeIdle = startEnd[2];
-  var endTimeIdle = startEnd[3];
-
-  var avgServiceT = avgTime(serviceTimeArr);
-  var avgTurnAroundT = avgTime(turnAroundTimeArr);
-  var avgWaitingT = avgTime(waitTimeArr);
-  var avgInterArrivalT = avgTime(interArrivalArr);
-
-  var waitTimeWhoWaitArr = avgWaitingTimeWhoWait(waitTimeArr);
-
-  console.log("Interarrival Time", interArrivalArr);
-  console.log("Waiting Time", waitTimeArr);
-  console.log("Wait Time for those who wait", waitTimeWhoWaitArr);
-  console.log("Response Time", responseTimeArr);
-  console.log("Turnaround Time", turnAroundTimeArr);
-
-  console.log("Start Time", startTime);
-  console.log("End Time", endTime);
-
-  console.log("Start Idle Time", startTimeIdle);
-  console.log("End Idle Time", endTimeIdle);
-
-  console.log("Average Service Time", avgServiceT);
-  console.log("Average TurnAround Time", avgTurnAroundT);
-  console.log("Average Waiting Time", avgWaitingT);
-  console.log("Average InterArrival Time", avgInterArrivalT);
-  console.log(
-    "Avg Utilization rate = ",
-    avgUtilizationRate(startTimeIdle, endTimeIdle, endTime[endTime.length - 1]),
-    "%"
-  );
-}
-
-// main()
 
 //================================ SIMMULATION ========================================
 
@@ -235,35 +115,6 @@ function arrivalTimeSim(interarrivalArr) {
   }
   return arrivalArr;
 }
-
-// function TimeSimulation(mu_lambda) {
-//   return Math.round(-Math.log(1 - Math.random()) * mu_lambda);
-// }
-
-// function Simulation(mins, lambda, mu) {
-  
-//   let clock = 0;
-//   let serviceTimeArray = [];
-//   let interArrivalTimeArray = [];
-//   let arrivalTimeArray = [];
-//   let clockArray = []
-
-// while (clock <= mins){
-//     clockArray.push(clock)
-//     let arrival_t = 0;
-//     let interarrival_t = TimeSimulation(lambda);
-    
-//     arrivalTimeArray.length > 0 ? arrival_t = arrivalTimeArray[arrivalTimeArray.length - 1] + interarrival_t :arrival_t = clock + interarrival_t;
-//     let service_t = TimeSimulation(mu);
-    
-//     interArrivalTimeArray.push(interarrival_t)
-//     arrivalTimeArray.push(arrival_t);
-//     serviceTimeArray.push(service_t);
-    
-//     clock += Math.min(interarrival_t, service_t)
-// }
-//   return [interArrivalTimeArray, serviceTimeArray, arrivalTimeArray, clockArray];
-// }
 
 //-----------------------------------RATE PARAMETER MODELS-----------------------------------
 //-------------------------------some used functions----------------------------------
@@ -521,8 +372,6 @@ export {
     avgTime,
     avgWaitingTimeWhoWait,
     avgUtilizationRate,
-    // mainSimmulation,
-    // arrivalTimeSim,
     MM1,
     MG1,
     GG1,
